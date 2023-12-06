1afb87f8840d0f1249988824b539d7ef0d0d3894
joannakolakowska <kolakowska.joanna.jk@gmail.com>
2023-12-06 18:47:32 +0100

    record type

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { fetchPoems } from "./services/apiPoems.jsx";
 import RootLayout from "./layouts/RootLayout";
 import ContainerLayout from "./layouts/ContainerLayout";
 import Home from "./pages/Home";
@@ -7,31 +8,38 @@ import PoemsList from "./pages/PoemsList";
 import CreditsList from "./pages/CreditsList";
 import PoemPage from "./pages/PoemPage";
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
 
   const [poemsData, setPoemsData] = useState([]);
 
-  let data = require(`./data/data.json`);
 
+  useEffect(() => {
+    fetchPoems.then((data: {record: Object[]}) => {
+      setPoemsData(data.record.poems);
+      console.log(data)
+    });
+  }, []);
+
+  let data = require(`./data/data.json`);
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<RootLayout />}>
-          <Route index element={<Home poemsData={data.poems} />} />
+          <Route index element={<Home poemsData={poemsData} />} />
           <Route element={<ContainerLayout />}>
             <Route
               path="lista"
-              element={<PoemsList poemsData={data.poems} />}
+              element={<PoemsList poemsData={poemsData} />}
             />
             <Route
               path="kredyty"
-              element={<CreditsList poemsData={data.poems} />}
+              element={<CreditsList poemsData={poemsData} />}
             />
           </Route>
-          <Route path=":id" element={<PoemPage poemsData={data.poems} />} />
+          <Route path=":id" element={<PoemPage poemsData={poemsData} />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/PoemPage.tsx
+++ src/pages/PoemPage.tsx
@@ -1,51 +1,63 @@
 import { useParams } from "react-router";
+import { useState, useEffect } from "react";
 
 interface Poem {
-    id: number;
-    title: string;
-    underTitle: string;
-    authors: string;
-    picture: string;
-    contents: string;
-    audio?: string;
-  };
-
-interface Props {
-    poemsData: Array<Poem>
-
+  id: number;
+  title: string;
+  underTitle: string;
+  authors: string;
+  picture: string;
+  contents: string;
+  audio?: string;
 }
 
-const PoemPage = ({poemsData}: Props) => {
+interface Props {
+  poemsData: Array<Poem>;
+}
 
-    const {id} = useParams()
-    const currentPoemId = Number(id);
+const PoemPage = ({ poemsData }: Props) => {
+  const [currentPoemData, setCurrentPoemData] = useState<Array<Poem>>([]);
+  const { id } = useParams();
+  const currentPoemId = Number(id);
+  console.log(poemsData);
 
+  useEffect(() => {
+    setCurrentPoemData(poemsData);
+  }, [poemsData]);
 
   return (
     <>
-    <div
-      className="poemHolder mainContainer"
-      style={{ backgroundImage: `url(${poemsData[currentPoemId].picture})` }}
-    >
-      <div className="shadow position-relative p-3 pt-5 p-lg-5 ">
-        <h3
-          className="poemTitle"
-          dangerouslySetInnerHTML={{ __html: poemsData[currentPoemId].title }}
-        ></h3>
-        <p
-          className={poemsData[currentPoemId].underTitle ? "poemUnderTitle" : "d-none"}
-          dangerouslySetInnerHTML={{ __html: poemsData[currentPoemId].underTitle }}
-        ></p>
-        <p
-          className="poemFontSizing italics pt-1 pt-lg-3 pb-1 pb-lg-4 "
-          dangerouslySetInnerHTML={{ __html: poemsData[currentPoemId].authors }}
-        ></p>
-        <p
-          className="poemFontSizing poemContents lh-base fw-bolder"
-          dangerouslySetInnerHTML={{ __html: poemsData[currentPoemId].contents }}
-        ></p>
+      <div
+        className="poemHolder mainContainer"
+        style={{ backgroundImage: `url(${poemsData[currentPoemId].picture})` }}
+      >
+        <div className="shadow position-relative p-3 pt-5 p-lg-5 ">
+          <h3
+            className="poemTitle"
+            dangerouslySetInnerHTML={{ __html: poemsData[currentPoemId].title }}
+          ></h3>
+          <p
+            className={
+              poemsData[currentPoemId].underTitle ? "poemUnderTitle" : "d-none"
+            }
+            dangerouslySetInnerHTML={{
+              __html: poemsData[currentPoemId].underTitle,
+            }}
+          ></p>
+          <p
+            className="poemFontSizing italics pt-1 pt-lg-3 pb-1 pb-lg-4 "
+            dangerouslySetInnerHTML={{
+              __html: poemsData[currentPoemId].authors,
+            }}
+          ></p>
+          <p
+            className="poemFontSizing poemContents lh-base fw-bolder"
+            dangerouslySetInnerHTML={{
+              __html: poemsData[currentPoemId].contents,
+            }}
+          ></p>
+        </div>
       </div>
-    </div>
     </>
   );
 };
