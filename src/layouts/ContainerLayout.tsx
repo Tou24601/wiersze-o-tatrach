3ceead77fd7ba59de495fe65ae2f11c7bd8a118f
joannakolakowska <kolakowska.joanna.jk@gmail.com>
2024-02-07 22:20:50 +0100

    header fix

--- src/layouts/ContainerLayout.tsx
+++ src/layouts/ContainerLayout.tsx
@@ -8,7 +8,7 @@ const ContainerLayout = () => {
         backgroundImage: `url(https://upload.wikimedia.org/wikipedia/commons/0/0e/Tatry%2C_zimowy_widok.jpg)`,
       }}
     >
-      <div className="shadow p-3 pt-5 p-lg-5">
+      <div className="shadow position-relative p-3 pt-5 p-lg-5">
         <Outlet />
       </div>
     </div>
